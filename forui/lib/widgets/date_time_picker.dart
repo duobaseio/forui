0eed49eacd156fad93f992170848785c64854dab
Matthias Ngeo <matthiasngeo@gmail.com>
2026-04-08 20:11:24 +0800

    Fix docs

--- forui/lib/widgets/date_time_picker.dart
+++ forui/lib/widgets/date_time_picker.dart
@@ -1,4 +1,10 @@
 /// {@category Widgets}
+///
+/// A date time picker that allows a date time to be selected. It is composed of multiple wheels.
+///
+/// Recommended for touch devices.
+///
+/// See https://forui.dev/docs/form/date-time-picker for working examples.
 library forui.widgets.date_time_picker;
 
 export '../src/widgets/picker/date_time/date_time_picker.dart';
