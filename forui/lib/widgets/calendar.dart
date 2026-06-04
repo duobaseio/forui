a88f301076d9b080799d462b5860c48866949ceb
Matthias Ngeo <matthiasngeo@gmail.com>
2026-06-04 21:33:36 +0800

    Add DateSelectionController

--- forui/lib/widgets/calendar.dart
+++ forui/lib/widgets/calendar.dart
@@ -10,5 +10,7 @@ export '../src/widgets/calendar/day/day_picker.dart' hide DayPicker;
 export '../src/widgets/calendar/header.dart' hide DualHeader, Header;
 export '../src/widgets/calendar/month/month.dart' hide Month;
 export '../src/widgets/calendar/month/month_picker.dart' hide MonthPicker;
+export '../src/widgets/calendar/date_selection_controller.dart'
+    hide InternalFDateSelectionControl, ProxyController;
 export '../src/widgets/calendar/year/year.dart' hide Year;
 export '../src/widgets/calendar/year/year_picker.dart' hide YearPicker;
