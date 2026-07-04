657089c9b3a3e8bd6d684d83f7dbf8aa4fd08104
Pante <Pante@users.noreply.github.com>
2026-07-04 16:12:00 +0000

    Prepare Forui for review

--- forui/lib/src/theme/theme.dart
+++ forui/lib/src/theme/theme.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:forui/src/foundation/focused_outline.dart';
 
 import 'package:meta/meta.dart';
 
 import 'package:forui/forui.dart';
+import 'package:forui/src/foundation/focused_outline.dart';
 
 part 'theme.design.dart';
 
