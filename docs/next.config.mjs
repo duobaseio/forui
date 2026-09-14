fad4cdac53cfade84dbed49bd2d62301eecade04
Matthias Ngeo <matthiasngeo@gmail.com>
2026-09-14 11:55:53 +0800

    Upgrade docs deps

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  agentRules: false,
   async rewrites() {
     return {
       beforeFiles: [
