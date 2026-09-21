0fc1eec19fa8abe7710d6905ae9361fdf5608c6a
Pante <Pante@users.noreply.github.com>
2026-09-21 15:05:10 +0000

    Prepare Forui for review

--- forui/test/src/widgets/resizable/divider_test.dart
+++ forui/test/src/widgets/resizable/divider_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/semantics.dart';
 import 'package:flutter/services.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:material_ui/material_ui.dart' hide VerticalDivider;
 
